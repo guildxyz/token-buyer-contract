d15983daf74ce593f85f41dcc6f754f259297b53
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-02-03 01:13:35 +0100

    Switch testing to Ethereum

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,8 +19,8 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
-        blockNumber: 38478920
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_ID}`,
+        blockNumber: 16542070
       }
     },
     mainnet: {
