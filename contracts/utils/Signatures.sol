83b5b4dd668d1c64917dcd8d236f92b17f864f66
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-02-06 21:49:11 +0100

    Bump Solidity version

--- contracts/utils/Signatures.sol
+++ contracts/utils/Signatures.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.18;
 
 contract Signatures {
     // bytes4(keccak256("isValidSignature(bytes32,bytes)")
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.18",
     settings: {
       optimizer: {
         enabled: true,
