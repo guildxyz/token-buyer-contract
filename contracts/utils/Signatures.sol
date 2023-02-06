0eef1674ed4c60309a9f023080f7715f049b8871
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-02-06 23:44:42 +0100

    Restrict access to isValidSignature

--- contracts/utils/Signatures.sol
+++ contracts/utils/Signatures.sol
@@ -1,18 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import { ITokenBuyer } from "../interfaces/ITokenBuyer.sol";
+
 contract Signatures {
     // bytes4(keccak256("isValidSignature(bytes32,bytes)")
     bytes4 internal constant MAGICVALUE = 0x1626ba7e;
 
-    /// @notice Returns whether the signature provided is valid for the provided hash.
+    /// @notice Accepts signatures from permit2, rejects otherwise.
     /// @param hash Hash of the data to be signed.
-    /// @param signature Signature byte array associated with _hash.
+    /// @param signature Signature byte array associated with hash.
     /// @return magicValue The function selector if the function passes.
-    function isValidSignature(bytes32 hash, bytes memory signature) public pure returns (bytes4 magicValue) {
-        // TODO: make this actually secure
+    function isValidSignature(bytes32 hash, bytes memory signature) public view returns (bytes4 magicValue) {
+        if (msg.sender == ITokenBuyer(address(this)).permit2()) return MAGICVALUE;
+
         hash;
         signature;
-        return MAGICVALUE;
+        return bytes4(0);
     }
 }
