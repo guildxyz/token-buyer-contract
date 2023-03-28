faa9f99e0373bc594603354ea2a3cad7fa44a787
Tomi_Ohl <ohlslager.tom@gmail.com>
2023-03-29 01:33:23 +0200

    Update deploy script

--- scripts/deploy-tokenbuyer.ts
+++ scripts/deploy-tokenbuyer.ts
@@ -3,12 +3,13 @@ import { ethers } from "hardhat";
 // Constructor arguments
 const universalRouter = "0x..."; // Uniswap Universal Router's address.
 const permit2 = "0x..."; // Permit2's address.
+const zkSyncBridge = "0x..."; // ZkSynk's bridge's address.
 const feeCollector = "0x..."; // The address that will receive a fee from the funds.
 const feePercentBps = 0; // The percentage of the fee expressed in basis points (e.g 500 for a 5% cut).
 
 async function main() {
   const TokenBuyer = await ethers.getContractFactory("TokenBuyer");
-  const tokenBuyer = await TokenBuyer.deploy(universalRouter, permit2, feeCollector, feePercentBps);
+  const tokenBuyer = await TokenBuyer.deploy(universalRouter, permit2, zkSyncBridge, feeCollector, feePercentBps);
 
   console.log(
     `Deploying contract to ${
@@ -19,7 +20,7 @@ async function main() {
   await tokenBuyer.deployed();
 
   console.log("TokenBuyer deployed to:", tokenBuyer.address);
-  console.log("Constructor arguments:", universalRouter, permit2, feeCollector, feePercentBps);
+  console.log("Constructor arguments:", universalRouter, permit2, zkSyncBridge, feeCollector, feePercentBps);
 }
 
 main().catch((error) => {
