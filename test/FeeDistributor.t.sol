4e1c388e8f52c8d7620ffd90ad60f04e2d256d66
PopcornPaws <lipschitz77@gmail.com>
2023-05-25 14:35:42 +0200

    Some more unit tests.

--- test/FeeDistributor.t.sol
+++ test/FeeDistributor.t.sol
@@ -19,7 +19,9 @@ contract FeeDistributorTest is Test {
 	address zeroAddress;
 	address oneAddress;
 	address payable feeCollector;
+	address payable invalidFeeCollector;
 	uint256 feeCollectorPrivateKey; 
+	uint256 invalidFeeCollectorPrivateKey; 
 	uint256 zeroBaseFeeEther;
 	uint256 oneBaseFeeEther;
 	uint96 feePercentBps;
@@ -27,6 +29,9 @@ contract FeeDistributorTest is Test {
 	function setUp() public {
 		feeCollectorPrivateKey = 0xFEEC011EC402;
 		feeCollector = payable(vm.addr(feeCollectorPrivateKey));
+		invalidFeeCollectorPrivateKey = 0xBAD;
+		invalidFeeCollector = payable(vm.addr(invalidFeeCollectorPrivateKey));
+
 		feePercentBps = 200;
 		zeroBaseFeeEther = 0.0005 ether;
 		oneBaseFeeEther = 0.0001 ether;
@@ -42,14 +47,40 @@ contract FeeDistributorTest is Test {
 	}
 
 	function test_CalculateFee() public {
+		// check initial values
 		assertEq(feeDistributorHarness.feeCollector(), feeCollector);
 		assertEq(feeDistributorHarness.feePercentBps(), feePercentBps);
 
-		// check baseFee
+		// check fee calculation (values computed using a python script)
 		assertEq(feeDistributorHarness.exposed_calculateFee(zeroAddress, 0.001 ether), 509803921570000);
 		assertEq(feeDistributorHarness.exposed_calculateFee(zeroAddress, 0.02 ether), 882352941180000);
-
 		assertEq(feeDistributorHarness.exposed_calculateFee(oneAddress, 0.001 ether), 117647058830000);
 		assertEq(feeDistributorHarness.exposed_calculateFee(oneAddress, 0.02 ether), 490196078440000);
+
+		// change fee base fee bps
+		vm.prank(feeCollector);
+		feeDistributorHarness.setFeePercentBps(300);
+		assertEq(feeDistributorHarness.feePercentBps(), 300);
+
+		// check fee calculation again
+		assertEq(feeDistributorHarness.exposed_calculateFee(zeroAddress, 0.001 ether), 514563106800000);
+		assertEq(feeDistributorHarness.exposed_calculateFee(zeroAddress, 0.02 ether), 1067961165050000);
+		assertEq(feeDistributorHarness.exposed_calculateFee(oneAddress, 0.001 ether), 126213592240000);
+		assertEq(feeDistributorHarness.exposed_calculateFee(oneAddress, 0.02 ether), 679611650490000);
+	}
+
+	function testFail_SetBaseFeeOnlyFeeCollector() public {
+		vm.prank(invalidFeeCollector);
+		feeDistributorHarness.setBaseFee(zeroAddress, 10 ether);
+	}
+
+	function testFail_SetFeeCollectorOnlyFeeCollector() public {
+		vm.prank(invalidFeeCollector);
+		feeDistributorHarness.setFeeCollector(payable(invalidFeeCollector));
+	}
+
+	function testFail_SetFeePercentBpsOnlyFeeCollector() public {
+		vm.prank(invalidFeeCollector);
+		feeDistributorHarness.setFeePercentBps(300);
 	}
 }
